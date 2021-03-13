5cab4d9f4dd5a479a78362b06c84fd42bcb199da
freecracy <12114073+freecracy@users.noreply.github.com>
2021-03-13 16:44:00 +0000

    qrcode

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,32 +1,53 @@
 const getConfig = require("vuepress-bar");
 const { nav, sidebar } = getConfig();
 module.exports = {
-    base: '/daily/',
-    title: '每日快讯',
-    host: '0.0.0.0',
-    port: 8080,
-    themeConfig: {
-        nav: [
-            { text: '首页', link: '/' },
-            { text: '主站', link: 'https://cn100800.tk' },
-            { text: 'mail', link: 'mailto:freecracy1024@gmail.com' },
-            { text: 'google', link: 'https://google.com.hk' },
+  base: "/daily/",
+  title: "每日快讯",
+  host: "0.0.0.0",
+  port: 8080,
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "主站", link: "https://cn100800.tk" },
+      { text: "mail", link: "mailto:freecracy1024@gmail.com" },
+      { text: "google", link: "https://google.com.hk" },
+    ],
+    lastUpdated: "最后更新",
+    sidebar,
+  },
+  plugins: [
+    ["@vuepress/active-header-links"],
+    ["@vuepress/back-to-top"],
+    ["@vuepress/medium-zoom"],
+    ["@vuepress/nprogress"],
+    ["@vuepress/pwa"],
+    ["vuepress-plugin-code-copy", true],
+    [
+      "qrcode",
+      {
+        labelText: "Mobile Read",
+        size: "small",
+      },
+    ],
+    [
+      "vuepress-plugin-toolbar",
+      {
+        opts: [
+          {
+            icon: "",
+            name: "文本展示",
+            popover: {
+              type: "text",
+              title: "纯文本说明",
+              text: "这是一个纯文本的内容展示，就是一段话",
+            },
+          },
         ],
-        lastUpdated: '最后更新',
-        sidebar,
-    },
-    plugins: [
-        ['@vuepress/active-header-links'],
-        ['@vuepress/back-to-top'],
-        ['@vuepress/medium-zoom'],
-        ['@vuepress/nprogress'],
-        ['@vuepress/pwa'],
-        ['vuepress-plugin-code-copy', true]
+      },
     ],
-    head: [
-        ['meta', { name: 'referrer', content: 'no-referrer' }],
-    ],
-    markdown: {
-        lineNumbers: true
-    }
-}
+  ],
+  head: [["meta", { name: "referrer", content: "no-referrer" }]],
+  markdown: {
+    lineNumbers: true,
+  },
+};
